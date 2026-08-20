803a57907b7ef1a5867e8b00853a5c143a10ebce
canonimpactsales-cpu <canon.impactsales@gmail.com>
2026-08-20 11:31:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'canon-focus-v3.0';
+const CACHE_NAME = 'canon-focus-v3.1';
 const IMG_CACHE = 'canon-focus-images-v1'; // jamais purgé aux montées de version
 const URLS_TO_CACHE = [
   '/canon-quiz/',
